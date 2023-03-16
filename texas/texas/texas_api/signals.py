de003791688ea6657d6c2e03642f0e56c0750009
Hannah Watsky <hannahlily24@gmail.com>
2023-03-15 22:13:18 -0400

    add log

--- texas/texas/texas_api/signals.py
+++ texas/texas/texas_api/signals.py
@@ -2,8 +2,10 @@ from django.db.models.signals import post_save
 from django.dispatch import receiver
 from texas_api.models import Game
 from texas.sio_server import sio_server
+from texas.logging import log
 
 
 @receiver(post_save, sender=Game)
 def update_game(sender, instance, **kwargs):
     sio_server.emit('my event', {'data': 'foobar'})
+    log.error('emitted a signal for my event.')
