a34ec0a10d12339d2f5fa833d756cfd07b6edde9
Hannah Watsky <hannahlily24@gmail.com>
2023-03-16 20:08:01 -0400

    add logs for socketio auth

--- texas/texas/texas/sio_server.py
+++ texas/texas/texas/sio_server.py
@@ -1,9 +1,11 @@
 import socketio
 
+from texas.logging import log
+
 
 sio_server = socketio.Server(async_mode='threading')
 
 
 @sio_server.event
 def connect(sid, environ, auth=''):
-    print('connect ', sid, auth)
+    log.error(f'sid: {sid} environ: {environ} auth: {auth}')
--- texas/texas/texas_api/signals.py
+++ texas/texas/texas_api/signals.py
@@ -2,10 +2,8 @@ from django.db.models.signals import post_save
 from django.dispatch import receiver
 from texas_api.models import Game
 from texas.sio_server import sio_server
-from texas.logging import log
 
 
 @receiver(post_save, sender=Game)
 def update_game(sender, instance, **kwargs):
     sio_server.emit('myevent', {'data': 'foobar'})
-    log.error('emitted a signal for myevent.')
