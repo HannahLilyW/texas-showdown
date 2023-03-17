f64f578e92dd953d517d578db0e3700709e11fc0
Hannah Watsky <hannahlily24@gmail.com>
2023-03-16 20:43:43 -0400

    fix typo

--- texas/texas/texas/sio_server.py
+++ texas/texas/texas/sio_server.py
@@ -11,5 +11,5 @@ def connect(sid, environ, auth=''):
     try:
         token = Token.objects.get(auth['token'])
     except Exception as e:
-        log.error(f'exception type: {type(exception)}')
-        log.error(f'exception: {exception}')
+        log.error(f'exception type: {type(e)}')
+        log.error(f'exception: {e}')
