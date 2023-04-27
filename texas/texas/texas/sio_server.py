dfd5405427a693904de1fa8deb074bfb2e58ecb9
Hannah Watsky <hannahlily24@gmail.com>
2023-04-26 22:59:31 -0400

    add socketio logging

--- texas/texas/texas/sio_server.py
+++ texas/texas/texas/sio_server.py
@@ -2,4 +2,4 @@ import socketio
 from texas.settings import config
 
 
-sio_server = socketio.AsyncServer(async_mode='asgi', cors_allowed_origins=['https://' + config['django']['hostname']])
+sio_server = socketio.AsyncServer(async_mode='asgi', cors_allowed_origins=['https://' + config['django']['hostname']], logger=True, engineio_logger=True)
