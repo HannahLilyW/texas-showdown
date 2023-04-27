bdaa4014bac69e8dca379b9bacd353a73d017f75
Hannah Watsky <hannahlily24@gmail.com>
2023-04-26 21:41:23 -0400

    update

--- install.sh
+++ install.sh
@@ -48,7 +48,7 @@ server {
     location /socket.io/ {
         proxy_set_header X-Forwarded-For \$proxy_add_x_forwarded_for;
         proxy_set_header Host \$host;
-        proxy_pass https://127.0.0.1:8443/;
+        proxy_pass https://127.0.0.1:8443/socket.io/;
         proxy_http_version 1.1;
         proxy_set_header Upgrade \$http_upgrade;
         proxy_set_header Connection "upgrade";
