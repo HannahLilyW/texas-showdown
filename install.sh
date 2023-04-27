5c6cd46e38c84d176fcae14c125851139a4e195d
Hannah Watsky <hannahlily24@gmail.com>
2023-04-26 21:22:11 -0400

    fix

--- install.sh
+++ install.sh
@@ -42,7 +42,7 @@ server {
 
     server_name $hostName www.$hostName;
 
-    location ~ ^/texas_api/(.*) {
+    location /texas_api/ {
         proxy_set_header X-Forwarded-For \$proxy_add_x_forwarded_for;
         proxy_set_header Host \$host;
         proxy_pass https://127.0.0.1:8443/;
