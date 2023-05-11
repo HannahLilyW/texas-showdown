89331792ed9b4941c69cc7b168d875db92cf02b8
Hannah Watsky <hannahlily24@gmail.com>
2023-05-10 22:12:55 -0400

    small styling change

--- vue-project/src/views/LandingPageView.vue
+++ vue-project/src/views/LandingPageView.vue
@@ -94,18 +94,19 @@ getCurrentGame();
         </div>
     </nav>
     <div class="rules">
+        <h1>Rules</h1>
         <p>
             Texas Showdown is a trick-taking card game. A trick-taking game is a game which consists of a certain number of rounds, or “tricks”, and each round has a winner who “takes the trick”. Texas Showdown is somewhat unusual among trick-taking games because the goal of the game is to take as few tricks as possible, and the winner of the game is the player that took the fewest tricks.
         </p>
-        <h3>Creating the Game</h3>
+        <h2>Creating the Game</h2>
         <p>
             A game can have three, four, five, or six players. The number of players is chosen by the creator of the game, and cannot be modified once the game has been created. A game can optionally include betting. If the creator of the game selects “High Stakes” on the game creation screen, the game will include betting. If the game creator selects “Just for Fun” on the game creation screen, the game will not include betting. Similar to the number of players, whether the game includes betting cannot be changed once the game has been created.
         </p>
-        <h3>Joining and Starting the Game</h3>
+        <h2>Joining and Starting the Game</h2>
         <p>
             Once the game has been created, any authenticated user that is not already in another game may join the game, until the required number of players have joined the game. Once the required number of players have joined, the owner of the game may start the game. Initially, the creator of the game is the owner of the game. If the creator of the game leaves the game before the game has started, then the next player still in the game that joined the earliest becomes the new owner of the game. During the time after the game has been created but before the game has been started, players may leave the game and other players may take their place. If all players leave a game before it has been started, that game is deleted from the database and does not show up in the list of completed games.
         </p>
-        <h3>Dealing</h3>
+        <h2>Dealing</h2>
         <p>
             Once the game is started, a 60-card deck consisting of the following cards is shuffled and evenly dealt into players’ hands:
         </p>
@@ -122,7 +123,7 @@ getCurrentGame();
         <p>
             Since there are 60 cards, the deck can be evenly split between three, four, five, or six players. Players do not know the contents of other players’ hands, except that all players know which player received the 0 card.
         </p>
-        <h3>Betting Round</h3>
+        <h2>Betting Round</h2>
         <p>
             Once the players have been dealt their initial cards, if the game includes betting, then a betting round commences. If the game does not include betting, this part is skipped. Each player is given $100 (in-game money, not real money) to bet with. The player who received the 0 card starts the betting. The order of play after that is determined by the order that the players joined the game. For example, in a three-player game, if the player who joined second received the zero card, then it would first be their turn, then the player who joined third, then the player who joined first.
         </p>
@@ -130,7 +131,7 @@ getCurrentGame();
             Betting is handled somewhat similarly to poker: the owner of the game may either “check” (which means they do not place a bet), or “open” (which means they bet some amount of money). If the owner of the game chose to check, then the next player may also either check or open. Until someone opens, each player may either check or open. If all of the players check, then the betting round ends with no money on the table. Once a player opens, then the next player must either “fold” (which means they are skipped for the rest of the betting round), “call” (which means they match the highest bet which was made so far), or “raise” (which means they increase their bet amount to higher than the previous highest bet). Each subsequent player must either fold, call, or raise until every player has either folded or bet the same amount. Players who have previously folded are skipped. Once all players who have not folded have bet the same amount, the betting round ends. Each player’s bet is transferred into the pot.
             If any player quits at any point during the betting round, or takes more than 60 seconds to take their turn, the game immediately ends, and that player loses. The remaining players win the game and evenly split the total amount that has been bet so far.
         </p>
-        <h3>Hand</h3>
+        <h2>Hand</h2>
         <p>
             Once the betting round is finished (if there was a betting round), then the first hand begins. Hands consist of multiple tricks. For each trick, each player plays one card from their hand. The loser of the trick “takes the trick”.
         </p>
@@ -142,7 +143,7 @@ getCurrentGame();
             The player who took the trick goes first in the next trick. They can play any card in their hand. The next player must play a card of the same color if they have it, otherwise they too can play any card in their hand. Play continues like this until there are no more cards in the players’ hands. Once all the players are out of cards, the hand is over.
             If any player quits, or takes more than 60 seconds to take their turn, the game immediately ends. The player that took too long loses, and the remaining players win.
         </p>
-        <h3>Subsequent Rounds and End of Game</h3>
+        <h2>Subsequent Rounds and End of Game</h2>
         <p>
             Once a hand is over, if betting is enabled, the player who took the least tricks that hand gets all the money in the pot. If multiple players tied for the least amount of tricks taken, they split the pot. The exception is that if one of those players had folded in the last betting round, they do not get any money from the pot. If every winner of the hand folded, then the money stays in the pot and is given to the winner(s) of the next hand. If there is no money in the pot and only one player has all the money, then the game is over and the player with all the money wins. Otherwise, a new betting round commences, followed by a new hand. Gameplay continues like this until only one player has all the money.
         </p>
