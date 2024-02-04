27a8dba876a3fc66152024439eac8ba55bd7162f
Hannah Watsky <hannahlily24@gmail.com>
2024-02-04 16:39:45 -0500

    add back arrow

--- vue-project/src/App.vue
+++ vue-project/src/App.vue
@@ -1,12 +1,14 @@
 <script setup lang="ts">
 import router from './router';
-import { RouterView } from 'vue-router';
+import { RouterView, useRoute } from 'vue-router';
 import { ref } from 'vue';
 import { username, updateOwnProfileInfo } from './services/api.js';
 import MenuIcon from './components/icons/IconMenu.vue';
+import ArrowLeftIcon from './components/icons/IconArrowLeft.vue';
 import MenuComponent from './components/MenuComponent.vue';
 
 let menu = ref();
+const route = useRoute();
 
 if (!username.value) {
   // Stop people from accidentally going to a page they need to be logged in for, before logging in
@@ -27,6 +29,9 @@ function showMenu() {
 
 <template>
   <header>
+    <div class="buttons-row" id="backButton">
+      <ArrowLeftIcon id="backButtonIcon" v-if="route.path != '/'" @click="goToLanding()"></ArrowLeftIcon>
+    </div>
     <div class="title-wrapper">
       <div id="title" class="point rye" @click="goToLanding()">
         WESTERN WAGER
@@ -53,6 +58,7 @@ header {
   background-color: var(--color-background-dark);
   box-shadow: 0 0 100px 60px rgba(0, 0, 0, 0.5);
   padding-right: 8px;
+  padding-left: 8px;
 }
 
 .container {
@@ -73,12 +79,19 @@ header {
 
 #title {
   font-size: 32px;
-  padding-left: 8px;
 }
 
-@media(max-width: 370px) {
+#backButton {
+  width: 32px;
+}
+
+#backButtonIcon {
+  cursor: pointer;
+}
+
+@media(max-width: 400px) {
   #title {
-    font-size: 20px;
+    font-size: 18px;
   }
 }
 </style>
